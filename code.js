340599500320d1e28b02848865bd610483bdfb00
M Charity <mastermilkx@gmail.com>
2022-10-12 14:54:27 -0400

    add 1.8.9 textures

--- code.js
+++ code.js
@@ -1,14 +1,55 @@
-//initial function for the app
-function init(){
-    console.log("Hello world!");
+// Javascript code for the VoxWorld JS web app
+// Code by Milk + Github Copilot
+
+
+
+/////////////    TEXTURE FUNCTIONS    //////////////
+
+
+//create a new dropdown texture item
+var ALL_TEXTURES = [];
+var CUR_TEXTURE_LIST = [];
+
+//import the list of textures from the JSON file (textures.json)
+function importTextures(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "textures.json", false);
+    request.send(null);
+    ALL_TEXTURES = JSON.parse(request.responseText);
+    // console.log(ALL_TEXTURES);
+    makeTextures();
 }
+
+//make dropdowns of the textures
+function makeTextures(numTexture=10){
+    
+
+
+
+}
+//change the texture ID's name and image
+function changeTexture(){
+
+}
+
+
+////////////     RENDERING FUNCTIONS    ////////////
+
 
 //change the values of the angle and zoom
 function changeSlideVal(id, value) {
-  var label = document.getElementById(id);
-  label.value = value;
+    var label = document.getElementById(id);
+    label.value = value;
 }
 function changeSlidePos(id, value){
     var pos = document.getElementById(id);
     pos.value = value;
+}
+
+
+
+//initial function for the app
+function init(){
+    console.log("Hello world!");
+    importTextures();
 }
